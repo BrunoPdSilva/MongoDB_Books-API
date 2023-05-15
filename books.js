b1223b3fe6d8882aee1a805bc693d0a069ea05ab
Bruno Peres <bruno_16515@hotmail.com>
2023-05-15 19:38:41 -0300

    Rota separada, get books separado para o arquivo books.js

--- books.js
+++ books.js
@@ -1,5 +1,13 @@
+import { db } from './index.js';
 import express from 'express';
 
 export const router = express.Router();
 
-router.get('/', (req, res) => res.send('Books API'))
+router.get('/', (req, res) => {
+  let books = [];
+
+  db.collection('books').find().sort({ author: 1 }).forEach(book => books.push(book))
+    .then(() => res.status(200).json(books))
+    .catch(err => res.status(500).json(err))
+    .finally(() => console.log('Finished'))
+})
--- index.js
+++ index.js
@@ -12,22 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
-let db
+export let db;
 
 connectToDB(err => {
-  if(!err) {
+  if (!err) {
     app.listen(PORT, () => console.log('listening on port 3000'));
     db = getDB();
-  } 
-})
-
-
-app.get('/books', (req, res) => {
-  let books = [];
-
-  db.collection('books').find().sort({ author: 1 }).forEach(book => books.push(book))
-    .then(() => res.status(200).json(books))
-    .catch(err => res.status(500).json(err))
-    .finally(() => console.log('Finished'))
+  }
 });
 
+app.use('/books', bookRoutes);
